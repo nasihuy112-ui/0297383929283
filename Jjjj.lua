a822d4ac3f71942350fc45b05f028f19e1fd5e8a
nasihuy112-ui <nasihuy112@gmail.com>
2026-07-06 03:02:00 +0700

    Create Jjjj.lua

---
+++ Jjjj.lua
@@ -0,0 +1,22 @@
+local ffi = require 'ffi'
+
+ffi.cdef[[
+    float fPlayerAimScale;
+]]
+
+local gta = ffi.load('GTASA')
+
+local original = gta.fPlayerAimScale
+gta.fPlayerAimScale = 0.0
+
+function main()
+    while true do
+        wait(-1)
+    end
+end
+
+function onScriptTerminate(script)
+    if script == thisScript() then
+        gta.fPlayerAimScale = original
+    end
+end
